04fb6b9eacebf568f1cfbabdce8139bbb7a3150f
Dillon Fagan <dillonfagan@pm.me>
2021-08-16 15:58:42 -0400

    Add Input component

---
+++ src/lib/Input.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let id;
+    export let type = "text";
+    export let label;
+    export let placeholder;
+</script>
+
+<div class="flex flex-col gap-2">
+    {#if label}
+        <label for={id} class="pl-1 text-gray-500 text-sm">{label}</label>
+    {/if}
+    <input {id} {type} {placeholder} class="bg-gray-100 px-4 py-2 rounded outline-none hover:ring-2 hover:ring-gray-200 focus:ring-2 focus:ring-gray-300" />
+</div>
--- src/routes/docs.svelte
+++ src/routes/docs.svelte
@@ -3,41 +3,75 @@
     import Row from "$lib/Row.svelte";
     import Button from "$lib/Button.svelte";
     import Code from "$lib/Code.svelte";
+    import Input from "$lib/Input.svelte";
 </script>
 
 <h1>Documentation</h1>
 <Column>
-    <h2>Button</h2>
     <Column>
-        <Row align="between">
-            <h3>Normal Button</h3>
-            <Button>Button</Button>
-        </Row>
-        <p>This is the default Button appearance.</p>
-        <Code source='<Button>Button</Button>' />
+        <h2>Button</h2>
+        <Column>
+            <Row align="between">
+                <h3>Normal Button</h3>
+                <Button>Button</Button>
+            </Row>
+            <p>This is the default Button appearance.</p>
+            <Code source='<Button>Button</Button>' />
+        </Column>
+        <Column>
+            <Row align="between">
+                <h3>Primary Button</h3>
+                <Button type="primary">Button</Button>
+            </Row>
+            <p>This is the primary Button appearance. Suitable for confirmations that respond to pressing Enter/Return.</p>
+            <Code source='<Button type="primary">Button</Button>' />
+        </Column>
+        <Column>
+            <Row align="between">
+                <h3>Danger Button</h3>
+                <Button type="danger">Button</Button>
+            </Row>
+            <p>This is the danger Button appearance. Suitable for confirmations that are destructive.</p>
+            <Code source='<Button type="danger">Button</Button>' />
+        </Column>
+        <Column>
+            <Row align="between">
+                <h3>Ghost Button</h3>
+                <Button type="ghost">Button</Button>
+            </Row>
+            <p>This is the ghost Button appearance. Suitable for cases where the button shouldn't draw attention to itself.</p>
+            <Code source='<Button type="ghost">Button</Button>' />
+        </Column>
     </Column>
     <Column>
-        <Row align="between">
-            <h3>Primary Button</h3>
-            <Button type="primary">Button</Button>
-        </Row>
-        <p>This is the primary Button appearance. Suitable for confirmations that respond to pressing Enter/Return.</p>
-        <Code source='<Button type="primary">Button</Button>' />
-    </Column>
-    <Column>
-        <Row align="between">
-            <h3>Danger Button</h3>
-            <Button type="danger">Button</Button>
-        </Row>
-        <p>This is the danger Button appearance. Suitable for confirmations that are destructive.</p>
-        <Code source='<Button type="danger">Button</Button>' />
-    </Column>
-    <Column>
-        <Row align="between">
-            <h3>Ghost Button</h3>
-            <Button type="ghost">Button</Button>
-        </Row>
-        <p>This is the ghost Button appearance. Suitable for cases where the button shouldn't draw attention to itself.</p>
-        <Code source='<Button type="ghost">Button</Button>' />
+        <h2>Input</h2>
+        <Column>
+            <Row align="between">
+                <h3>Text Input</h3>
+                <Input />
+            </Row>
+            <Code source='<Input />' />
+        </Column>
+        <Column>
+            <Row align="between">
+                <h3>Adding a label</h3>
+                <Input label="Username" />
+            </Row>
+            <Code source='<Input label="Username" />' />
+        </Column>
+        <Column>
+            <Row align="between">
+                <h3>Adding a placeholder</h3>
+                <Input label="Username" placeholder="jsmith" />
+            </Row>
+            <Code source='<Input label="Username" placeholder="jsmith" />' />
+        </Column>
+        <Column>
+            <Row align="between">
+                <h3>Password Input</h3>
+                <Input type="password" label="Password" placeholder="Pa$$word" />
+            </Row>
+            <Code source='<Input type="password" label="Password" placeholder="Pa$$word" />' />
+        </Column>
     </Column>
 </Column>
